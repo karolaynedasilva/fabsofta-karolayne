74aebee056929cdddb8e8fe6c76c9562ee3bcf65
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2025-03-18 21:50:45 -0300

    feat: revisao

--- revisao/src/main/java/br/univille/App.java
+++ revisao/src/main/java/br/univille/App.java
@@ -1,13 +1,23 @@
 package br.univille;
+import br.univille.entity.Cidade;
+import  br.univille.entity.Pessoa;
+
 
 public class App {
 
     public static void main(String[] args) {
         Pessoa mariazinha = new Pessoa("Mariazinha");
         Pessoa zezinho = new Pessoa();
+        Cidade jlle = new Cidade();
+
         zezinho.setNome("Zezinho");
+        zezinho.setCidade(jlle);
+        jlle.setNome("Joinville");
+        jlle.setEstado("Santa Catarina");
 
         System.out.println(mariazinha);
         System.out.println(zezinho);
+
+
     }
 }
--- revisao/src/main/java/br/univille/Pessoa.java
+++ revisao/src/main/java/br/univille/Pessoa.java
@@ -1,30 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 package br.univille;
 
-public class Pessoa {
-    
-    private String nome;
 
-    //obrigando a passar o nome
-    public Pessoa(String nome) {
-        //this referencia a classe
-        this.nome = nome;
-    }
-    //torna opcional passar o nome
-    public Pessoa() {
+class Pessoa {
 
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    //Sobreescrita de método 
-    @Override
-    public String toString() {
-        return getNome();
-    }
 }
