d9fd76363b7c91e93e6799f2717e9eff9fa00c94
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2025-04-01 21:59:37 -0300

    feat: inicio projeto

---
+++ projfabsoftagenda/src/test/java/br/univille/projfabsoftagenda/ProjfabsoftagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
