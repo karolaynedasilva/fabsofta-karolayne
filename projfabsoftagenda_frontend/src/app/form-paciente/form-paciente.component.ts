e3b5f38d6fa0f52a5a5c2147d633c9942a278675
Karolayne <99550771+karolaynedasilva@users.noreply.github.com>
2025-06-19 15:35:31 +0000

    feat: ajuste nas validações

--- projfabsoftagenda_frontend/src/app/form-paciente/form-paciente.component.ts
+++ projfabsoftagenda_frontend/src/app/form-paciente/form-paciente.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Paciente } from '../model/paciente';
 import { PacienteService } from '../service/paciente.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
@@ -16,7 +16,7 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 })
 export class FormPacienteComponent {
     paciente: Paciente = new Paciente();
-
+    @ViewChild('formPaciente') formPaciente!: NgForm;
     constructor(
       private pacienteService: PacienteService,
       private router: Router,
@@ -31,10 +31,15 @@ export class FormPacienteComponent {
       }
     }
 
-    salvar(){
+    salvar() {
+    if (this.formPaciente.valid) {
       this.pacienteService.savePaciente(this.paciente)
-          .subscribe( res => {
-            this.router.navigate(['pacientes']);
-          });
+        .subscribe(res => {
+          this.router.navigate(['pacientes']);
+        });
+    } else {
+      // marca todos os campos como tocados para exibir mensagens de erro
+      this.formPaciente.form.markAllAsTouched();
     }
+  }
 }
